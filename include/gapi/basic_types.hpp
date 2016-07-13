470631f43e83ac093639af2fb74d608919372746
Keith Hammond <keithammond@gmail.com>
2016-07-14 00:43:33 +1200

    changed to [u]int_least[bits]_t

--- include/gapi/basic_types.hpp
+++ include/gapi/basic_types.hpp
@@ -8,10 +8,10 @@ namespace gapi{
 	using GLubyte = unsigned char;
 	using GLfloat = float;
 	using GLdouble = double;
-	using GLushort = std::uint16_t;
-	using GLshort = std::int16_t;
-	using GLuint = std::uint32_t;
-	using GLint = std::int32_t;
+	using GLushort = std::uint_least16_t;
+	using GLshort = std::int_least16_t;
+	using GLuint = std::uint_least32_t;
+	using GLint = std::int_least32_t;
 	using GLboolean = bool;
 	using GLsizei = GLint;
 	using GLbitfield = GLuint;
