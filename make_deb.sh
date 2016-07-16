93586ac4b701b407a8164bd974feb1abb46bb757
Keith Hammond <keithammond@gmail.com>
2016-07-16 22:32:54 +1200

    removed debian packaging stuff. seems superflous for the git repo

--- make_deb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo_do() {
-	tput setaf 3; echo "$@"; tput sgr0;
-	$@
-}
-
-echo_do make install BUILD_TYPE=release DESTDIR=`pwd`/gapi_0.1-1/usr/local
-echo_do dpkg-deb --build gapi_0.1-1
